47217b3d3eac8cf362b17e0f87b841c7877bf371
Jean <jeanbarrossilva@outlook.com>
2020-09-12 12:34:40 -0400

    Add default take off and landing time values to AcquisitionCategory.FlightTicket

--- laurafoundation/src/main/java/com/jeanbarrossilva/laurafoundation/AcquisitionCategory.kt
+++ laurafoundation/src/main/java/com/jeanbarrossilva/laurafoundation/AcquisitionCategory.kt
@@ -7,7 +7,10 @@ import java.time.LocalDateTime
 sealed class AcquisitionCategory(@StringRes val name: Int, @DrawableRes val icon: Int) {
     object GeneralAcquisition : AcquisitionCategory(R.string.acquisition_category_general, R.drawable.ic_question)
 
-    data class FlightTicket(val takeOff: LocalDateTime, val landing: LocalDateTime)
+    data class FlightTicket(
+        val takeOff: LocalDateTime = LocalDateTime.now(),
+        val landing: LocalDateTime = LocalDateTime.from(takeOff).plusDays(1)
+    )
         : AcquisitionCategory(R.string.acquisition_category_flight_ticket, R.drawable.ic_local_airport) {
         val landingTime = "${landing.hour}:${landing.minute}"
         val takeOffTime = "${takeOff.hour}:${takeOff.minute}"
